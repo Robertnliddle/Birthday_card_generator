5d55dc4bde2e3df8df09f795b5e1649e8911b635
Robertnliddle <143751397+Robertnliddle@users.noreply.github.com>
2023-09-04 21:36:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#Birthday card generator
+
+input_value = input("What is the recipient's name? ")
+input_number = input("How old will the recipient be turning? ")
+input_senders_name = input("Who is sending the birthday card? ")
+
+print("----------------------------")
+
+print(
+    f"Happy birthday {input_value}, you are now turning {input_number} years old! "
+)
+
+print("We wish you the best birthday ever with a lot of presents and cakes!")
+
+print("We will visit you as soon as we can and give you our presents and i already know that you are gonna like them!:P")
+
+print("With love and wishes")
+print(input_senders_name)
+
+print("----------------------------")
+
